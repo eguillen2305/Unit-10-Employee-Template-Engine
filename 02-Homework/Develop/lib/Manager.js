a28502952c455d6ace087095856a5dac70fee2f0
Eduardo Guillen <eguillen2035@gmail.com>
2020-06-21 15:51:12 -0500

    Updated manager.js - Const and class

--- 02-Homework/Develop/lib/Manager.js
+++ 02-Homework/Develop/lib/Manager.js
@@ -1,1 +1,18 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const employee = require('./Employee');
+
+class Manager extends Employee {
+	constructor(name, id, email, office) {
+		super(name, id, email);
+		this.office = office;
+	}
+
+	getOfficeNumber() {
+		return this.office;
+	}
+
+	getRole() {
+		return 'Manager';
+	}
+}
+module.exports = Manager;
