fc0c2f3da2a4f397f8a1afb4e10e0dfc8ef2d544
Eduardo Guillen <eguillen2035@gmail.com>
2020-06-21 16:06:57 -0500

    Added async prompts/questions.

--- 02-Homework/Develop/app.js
+++ 02-Homework/Develop/app.js
@@ -1,15 +1,23 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
-const render = require("./lib/htmlRenderer");
+const render = require('./lib/htmlRenderer');
 
+async function askQuestions(employeeList) {
+	const baseQuestions = [
+		'What is the employee name?',
+		'What is the employee id?',
+		'What is the email of the employee?',
+		'What is the role of the employee?'
+	];
+};
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
